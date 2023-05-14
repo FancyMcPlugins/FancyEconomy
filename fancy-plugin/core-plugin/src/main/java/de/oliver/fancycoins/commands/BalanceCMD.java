96b0b59d839d2bbdf1fa0c08a69f7f551621b520
mani123 <70480001+mani1232@users.noreply.github.com>
2023-05-14 20:45:16 +0200

    changes in cmds

--- fancy-plugin/core-plugin/src/main/java/de/oliver/fancycoins/commands/BalanceCMD.java
+++ fancy-plugin/core-plugin/src/main/java/de/oliver/fancycoins/commands/BalanceCMD.java
@@ -3,15 +3,26 @@ package de.oliver.fancycoins.commands;
 import dev.jorel.commandapi.annotations.Alias;
 import dev.jorel.commandapi.annotations.Command;
 import dev.jorel.commandapi.annotations.Default;
+import dev.jorel.commandapi.annotations.Permission;
 import org.bukkit.entity.Player;
 
 @Command("balance")
 @Alias({"money"})
+@Permission("fancycoins.balance")
 public class BalanceCMD {
 
     @Default
     public static void balance(
             Player player
+    ) {
+
+    }
+
+    @Default
+    @Permission("fancycoins.balance.vaults")
+    public static void balance(
+            Player player,
+            String vault
     ) {
 
     }
